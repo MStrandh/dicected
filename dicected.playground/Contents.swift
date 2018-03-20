35e70699cea011f94e75843c7d640bc57e211517
Magnus Strandh <magnus.strandh@king.com>
2018-03-20 15:49:43 +0100

    Basics

--- dicected.playground/Contents.swift
+++ dicected.playground/Contents.swift
@@ -6,9 +6,29 @@ func probabilityToHit(k : Double) -> Double {
 	return Double(7 - k) / 6.0;
 }
 
-func probabilityToHitWithRerollAllFailed(k: Double) -> Double {
+func probabilityToHitWithRerollAll(k: Double) -> Double {
 	return ((7 - k) * 6 + (k - 1) * (7 - k)) / 36.0;
 }
 
+func probabilityToHitWithLimitedReroll(k: Double) -> Double {
+	return ((7 - k) / 6) + ((7 - k) / 36);// + ((7 - k) / 6);
+}
+
+func prettyPrint(probability: Double) -> Void {
+	print("\(String(format: "%.3f", probability))");
+}
+
 var toHit : Double = probabilityToHit(k: 3);
-var reRollAll : Double = probabilityToHitWithRerollAllFailed(k: 3);
+
+print("--- To Hit with reroll for all misses ---");
+for diceSide in 2...6 {
+	let reRollAll : Double = probabilityToHitWithRerollAll(k: Double(diceSide));
+	prettyPrint(probability: reRollAll);
+}
+
+print("--- To Hit with rerolling ones ---");
+for diceSide in 2...6 {
+	let reRollAll : Double = probabilityToHitWithLimitedReroll(k: Double(diceSide));
+	prettyPrint(probability: reRollAll);
+}
+
