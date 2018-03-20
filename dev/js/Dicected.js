449d492443f2e1110151fa52e390b6c97afb7da0
Magnus Strandh <magnus.strandh@king.com>
2018-03-20 16:15:36 +0100

    To Hit probabilities

--- dev/js/Dicected.js
+++ dev/js/Dicected.js
@@ -1,1 +1,21 @@
 console.log("Dicected");
+
+function probabilityToHitWithRerollAll(k) {
+	return ((7 - k) * 6 + (k - 1) * (7 - k)) / 36.0;
+}
+
+function probabilityToHitWithLimitedReroll(k) {
+	return ((7 - k) / 6) + ((7 - k) / 36);
+}
+
+
+console.log("--- To Hit rerolling all --- ");
+for(var i = 2; i <= 6; i++) {
+	console.log(i + ": " + probabilityToHitWithRerollAll(i));
+}
+
+
+console.log("--- To Hit rerolling ones --- ");
+for(var i = 2; i <= 6; i++) {
+	console.log(i + ": " + probabilityToHitWithLimitedReroll(i));
+}
