854049135cdd6083f1e105ca8322bf98ebc915f0
Magnus Strandh <magnus.strandh@king.com>
2018-05-16 14:50:40 +0200

    Added json data

--- dev/js_app/DicectedArmyApp.js
+++ dev/js_app/DicectedArmyApp.js
@@ -4,4 +4,8 @@
 	var weapon = new Weapon();
 	console.log("Weapon AP: " + weapon.armourPiercing);
 	console.log("Weapon num shots: " + weapon.numShots);
+
+	$.getJSON("assets/data/wh40k/orks/ranged.json", function(json) {
+		console.log(json);
+	});
 })();
